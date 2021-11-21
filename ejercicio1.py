ab3131325498c6e7938f98e5a9f5ca24e7d8ab7e
ady6 <82877241+ady6@users.noreply.github.com>
2021-11-21 21:34:09 +0100

    consumo archivo csv con pandas

---
+++ ejercicio1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+url=""
+df = pd.read_csv('https://raw.githubusercontent.com/ady6/MachineLearning-Webscrapping/master/games.csv')
+df.head()
+
+
+# In[7]:
+
+
+print(df)
+
